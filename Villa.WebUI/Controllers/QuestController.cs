dcbd05f54da6fe79a1243a96a3a15ea7c0d6784d
Metin Mescioğlu <mmescioglu@hotmail.com>
2024-04-28 02:37:50 +0300

    Create Deal Başladı

--- Villa.WebUI/Controllers/QuestController.cs
+++ Villa.WebUI/Controllers/QuestController.cs
@@ -2,17 +2,17 @@
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
 using Villa.Business.Abstract;
-using Villa.Dto.Dtos.BannerDtos;
+using Villa.Dto.Dtos.QuestDtos;
 using Villa.Entity.Entities;
 
 namespace Villa.WebUI.Controllers
 {
-    public class BannerController : Controller
+    public class QuestController : Controller
     {
-        private readonly IBannerService _bannerService;
+        private readonly IQuestService _bannerService;
         private readonly IMapper _mapper;
 
-        public BannerController(IBannerService bannerService, IMapper mapper)
+        public QuestController(IQuestService bannerService, IMapper mapper)
         {
             _bannerService = bannerService;
             _mapper = mapper;
@@ -21,42 +21,42 @@ namespace Villa.WebUI.Controllers
         public async Task<IActionResult> Index()
         {
             var values = await _bannerService.TGetAllAsync();
-            var bannerList = _mapper.Map<List<ResultBannerDto>>(values);
+            var bannerList = _mapper.Map<List<ResultQuestDto>>(values);
             return View(bannerList);
 
         }
 
 
-        public async Task<IActionResult> DeleteBanner(ObjectId id)
+        public async Task<IActionResult> DeleteQuest(ObjectId id)
         {
             await _bannerService.TDeleteAsync(id);
             return RedirectToAction("Index");
         }
 
-        public IActionResult CreateBanner()
+        public IActionResult CreateQuest()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateBanner(CreateBannerDto createBannerDto)
+        public async Task<IActionResult> CreateQuest(CreateQuestDto createQuestDto)
         {
-            var newBanner = _mapper.Map<Banner>(createBannerDto);
-            await _bannerService.TCreateAsync(newBanner);
+            var newQuest = _mapper.Map<Quest>(createQuestDto);
+            await _bannerService.TCreateAsync(newQuest);
             return RedirectToAction("Index");
         }
 
-        public async Task<IActionResult> UpdateBanner(ObjectId id)
+        public async Task<IActionResult> UpdateQuest(ObjectId id)
         {
             var value = await _bannerService.TGetByIdAsync(id);
-            var updateBanner = _mapper.Map<UpdateBannerDto>(value);
-            return View(updateBanner);
+            var updateQuest = _mapper.Map<UpdateQuestDto>(value);
+            return View(updateQuest);
         }
 
         [HttpPost]
-        public async Task<IActionResult> UpdateBanner(UpdateBannerDto updateBannerDto)
+        public async Task<IActionResult> UpdateQuest(UpdateQuestDto updateQuestDto)
         {
-            var banner = _mapper.Map<Banner>(updateBannerDto);
+            var banner = _mapper.Map<Quest>(updateQuestDto);
             await _bannerService.TUpdateAsync(banner);
             return RedirectToAction("Index");
         }
--- Villa.WebUI/Mapping/GeneralMapping.cs
+++ Villa.WebUI/Mapping/GeneralMapping.cs
@@ -1,5 +1,13 @@
 ﻿using AutoMapper;
 using Villa.Dto.Dtos.BannerDtos;
+using Villa.Dto.Dtos.ContactDtos;
+using Villa.Dto.Dtos.CounterDtos;
+using Villa.Dto.Dtos.DealDtos;
+using Villa.Dto.Dtos.FeatureDtos;
+using Villa.Dto.Dtos.MessageDtos;
+using Villa.Dto.Dtos.ProductDtos;
+using Villa.Dto.Dtos.QuestDtos;
+using Villa.Dto.Dtos.VideoDtos;
 using Villa.Entity.Entities;
 
 namespace Villa.WebUI.Mapping
@@ -11,6 +19,38 @@ namespace Villa.WebUI.Mapping
             CreateMap<ResultBannerDto, Banner>().ReverseMap();
             CreateMap<UpdateBannerDto, Banner>().ReverseMap();
             CreateMap<CreateBannerDto, Banner>().ReverseMap();
+
+            CreateMap<ResultContactDto, Contact>().ReverseMap();
+            CreateMap<UpdateContactDto, Contact>().ReverseMap();
+            CreateMap<CreateContactDto, Contact>().ReverseMap();
+
+            CreateMap<ResultCounterDto, Counter>().ReverseMap();
+            CreateMap<UpdateCounterDto, Counter>().ReverseMap();
+            CreateMap<CreateCounterDto, Counter>().ReverseMap();
+
+            CreateMap<ResultDealDto, Deal>().ReverseMap();
+            CreateMap<UpdateDealDto, Deal>().ReverseMap();
+            CreateMap<CreateDealDto, Deal>().ReverseMap();
+
+            CreateMap<ResultFeatureDto, Feature>().ReverseMap();
+            CreateMap<UpdateFeatureDto, Feature>().ReverseMap();
+            CreateMap<CreateFeatureDto, Feature>().ReverseMap();
+
+            CreateMap<ResultMessageDto, Message>().ReverseMap();
+            CreateMap<UpdateMessageDto, Message>().ReverseMap();
+            CreateMap<CreateMessageDto, Message>().ReverseMap();
+
+            CreateMap<ResultProductDto, Product>().ReverseMap();
+            CreateMap<UpdateProductDto, Product>().ReverseMap();
+            CreateMap<CreateProductDto, Product>().ReverseMap();
+
+            CreateMap<ResultQuestDto, Quest>().ReverseMap();
+            CreateMap<UpdateQuestDto, Quest>().ReverseMap();
+            CreateMap<CreateQuestDto, Quest>().ReverseMap();
+
+            CreateMap<ResultVideoDto, Video>().ReverseMap();
+            CreateMap<UpdateVideoDto, Video>().ReverseMap();
+            CreateMap<CreateVideoDto, Video>().ReverseMap();
 
         }
     }
